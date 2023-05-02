79c1f4ab0def6cd70a8313f8fc60d215657463aa
Tabita Costa <tabitacostagc@gmail.com>
2023-05-02 11:35:17 -0300

    initial commit

---
+++ tarefas_hora/index.js
@@ -0,0 +1,85 @@
+const getHours = () => {
+    const clock = document.getElementsByClassName('clock')[0]
+    const date = new Date()
+    const hours = date.getHours()
+    const minutes = date.getMinutes()
+    const seconds = date.getSeconds()
+    const hour = hours < 10 ? `0${hours}` : hours
+    const minute = minutes < 10 ? `0${minutes}` : minutes
+    const second = seconds < 10 ? `0${seconds}` : seconds
+    clock.innerHTML = `${hour}:${minute}:${second}`
+  }
+  
+  setInterval(() => {
+    getHours()
+  }, 1000);
+  
+
+  let contador = 0;
+let input = document.getElementById("inputTarefa");
+let btnAdd = document.getElementById("btn-add");
+let main = document.getElementById("areaLista");
+
+function addTarefa() {
+  //PEGAR O VALOR DIGITADO NO INPUT
+  let valorInput = input.value;
+
+  //SE NÃO FOR VAZIO, NEM NULO, NEM INDEFINIDO
+  if (valorInput !== "" && valorInput !== null && valorInput !== undefined) {
+    ++contador;
+
+    let novoItem = `<div id="${contador}" class="item">
+        <div onclick="marcarTarefa(${contador})" class="item-icone">
+            <i id="icone_${contador}" class="mdi mdi-circle-outline"></i>
+        </div>
+        <div onclick="marcarTarefa(${contador})" class="item-nome">
+            ${valorInput}
+        </div>
+        <div class="item-botao">
+            <button onclick="deletar(${contador})" class="delete"><i class="mdi mdi-delete"></i> Deletar</button>
+        </div>
+    </div>`;
+
+    //ADICIONAR NOVO ITEM NO MAIN
+    main.innerHTML += novoItem;
+
+    //ZERAR OS CAMPINHOS
+    input.value = "";
+    input.focus();
+  }
+}
+
+function deletar(id) {
+  var tarefa = document.getElementById(id);
+  tarefa.remove();
+}
+
+function marcarTarefa(id) {
+  var item = document.getElementById(id);
+  var classe = item.getAttribute("class");
+  console.log(classe);
+
+  if (classe == "item") {
+    item.classList.add("clicado");
+
+    var icone = document.getElementById("icone_" + id);
+    icone.classList.remove("mdi-circle-outline");
+    icone.classList.add("mdi-check-circle");
+
+    item.parentNode.appendChild(item);
+  } else {
+    item.classList.remove("clicado");
+
+    var icone = document.getElementById("icone_" + id);
+    icone.classList.remove("mdi-check-circle");
+    icone.classList.add("mdi-circle-outline");
+  }
+}
+
+input.addEventListener("keyup", function (event) {
+  //SE TECLOU ENTER (13)
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    btnAdd.click();
+  }
+});
---
+++ tarefas_hora/readme.md
@@ -0,0 +1,3 @@
+# Lista de tarefas e hora exata em JS
+
+Fiz essa lista de tarefas e o relogio em javascript, olhando tutoriais pois ainda não domino essa linguagem e estou apenas me aventurando e tentando entender um pouco como ela funciona, essa aplicação ficou um pouco lenta na hora de recarregar a pagina acredito que ficou pesada por eu não saber exatamente como usar a linguagem de forma adequada, mas espero que com o tempo eu consiga adiquirir conheciemento o suficiente para refatorar esse codigo.
